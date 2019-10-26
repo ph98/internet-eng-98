ebede508ced757ee40a5b6340fb73b6f339bf7d1
parham heidari <heidari1377@gmail.com>
2019-10-26 15:16:31 +0330

    init asp dot net core

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace internet_engineering.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
